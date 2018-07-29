777b184325bf856b5b22f20cb2d0e01fc4c8b83d
Zack <Zackjohnlee@Not-here.local>
2018-07-28 23:00:20 -0700

    positioned modal elements

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,30 +6,31 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 const Modal = (props) => {
     return (
         <div id="modal">
-            <div id="modalToggle">
-                <input 
-                    id="modalCheck" 
-                    type="checkbox"
-                    name="modalOpen"
-                    checked={props.modalOpen}
-                    onChange={props.handleModal}
-                />
-                <label htmlFor="modalCheck"/>
-            </div>
-            
-            <div id="modalImage" >
-                <LazyLoadImage
-                    src={require("../images/"+ props.collection + "/" + props.mainSrc + ".jpg")}
-                    effect="blur"
-                    wrapperClassName="image"
-                />
-            </div>
-            <div id="galleryNav">
-                <button id="adv" onClick={props.navGallery}></button>
-                <button id="dec" onClick={props.navGallery}></button>
-            </div>
-            <div id="modalDesc">
-                <p>{props.desc}</p>
+            <div id="modalContainer">
+                <div id="modalToggle">
+                    <input 
+                        id="modalCheck" 
+                        type="checkbox"
+                        name="modalOpen"
+                        checked={props.modalOpen}
+                        onChange={props.handleModal}
+                    />
+                    <label htmlFor="modalCheck"/>
+                </div>
+                <div id="modalImage" >
+                    <LazyLoadImage
+                        src={require("../images/"+ props.collection + "/" + props.mainSrc + ".jpg")}
+                        effect="blur"
+                        wrapperClassName="image"
+                    />
+                </div>
+                <div id="galleryNav">
+                    <button id="dec" onClick={props.navGallery}/>
+                    <button id="adv" onClick={props.navGallery}/>
+                </div>
+                <div id="modalDesc">
+                    <p>{props.desc}</p>
+                </div>
             </div>
         </div>
     );
