371ffddb27fd952434af6a0884a5e542f458a73f
Zack <Zackjohnlee@Not-here.local>
2018-08-23 16:34:54 -0700

    revised gitignore

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+    stripe: {
+      apiKey: 'pk_test_2jkjdwGUg1O1t3sUqiENYiJP',
+      checkoutUrl: 'https://le43h27bpb.execute-api.us-west-1.amazonaws.com/dev/orders',
+      productsUrl: 'https://le43h27bpb.execute-api.us-west-1.amazonaws.com/dev/skus',
+      currency: 'USD',
+    },
+  };
+  
+  export default config;
